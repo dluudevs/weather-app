28300406d16b79dec3f9b232bf5b48950e2f73c4
Derek <derekluu90@gmail.com>
2020-05-14 17:40:30 -0400

    added input and placed forecast inside of geocode callback function

--- app.js
+++ app.js
@@ -2,18 +2,30 @@
 
 // local file requires '/'
 const geocode = require('./utils/geocode')
-const forecast = require('./utils/weather')
+const forecast = require('./utils/forecast')
 
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
-
-// callback with two parameters error and response is good practice and strongly recommended
-geocode('Toronto', (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
+const address = process.argv[2]
+if (!address){
+  console.log('Please provide an address')
+} else {
+  // callback with two parameters error and response (in this function response is a destructured object) is good practice and strongly recommended
+  // add default value of empty object for response, without the empty object the app will crash if the value is undefined (ie., invalid location searched)
+  // the geocode function crashes the app as it tries to destructure undefined
+  geocode(address, (error, { longitude, latitude, location } = {}) => {
+    // if error, return to exit the function
+    if (error) {
+      return console.log(error)
+    }
+    // for all other instances where this is no error, make a call to forecast
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error){
+        return console.log(error)
+      }
+      console.log(location)
+      console.log(forecastData)
+    })
+  })
+}
 
 
 
--- utils/geocode.js
+++ utils/geocode.js
@@ -5,16 +5,17 @@ const geocode = (address, callback) => {
   // eg., '?' would crash the app, encoder would encode it to '%3F'
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiZGVsdnYiLCJhIjoiY2szejRmdHFjMWUyajNlcGRwb2dncml0aSJ9.BjzlJbeyH4qtVzr8ns9gDA&limit=1`
   request({ url: url, json: true }, (error, response) => {
+    const { features } = response.body
     if (error) {
       callback('Unable to connect to location services!', undefined)
-    } else if (!response.body.features.length) {
+    } else if (!features.length) {
       callback('Unable to find location. Try another search', undefined)
     } else {
-      const [ longitude, latitude ] = response.body.features[0].center
+      const [ longitude, latitude ] = features[0].center
       callback(undefined, {
         longitude,
         latitude,
-        location: response.body.features[0].place_name
+        location: features[0].place_name
       })
     }
   })
