aeafe2facc932052b49d06cb0e7175b2e32ac757
Derek <derekluu90@gmail.com>
2020-05-13 22:05:28 -0400

    created utility functions for api requests

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
 // TODO: Re-watch Lecture 30 after weather-app is complete 
-const request = require('request')
-const url = 'http://api.weatherstack.com/current?access_key=47199bc362cf473b97d334e5179d307b&query=37.8267,-122.4233&units=f'
 
-// first argument is options object, url must be provided
-// json option converts data to json without having to use JSON.parse()
-request({url, json: true}, (error, response) => { 
-  // error only gets passed to callback when network request fails. this is called a low level error
-  if(error){
-    console.log('Unable to connect to weather service!')
-  // else if for situations where a response with an error is passed. ie., when no coordinates is provided to the query
-  } else if (response.body.error){
-    console.log('Unable to find location')
-  } else {
-    const weather = response.body.current
-    console.log(`${weather.weather_descriptions[0]}. It is currently ${weather.temperature} degrees out. It feels like ${weather.feelslike} degrees out`)
-  }
+// local file requires '/'
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/weather')
+
+forecast(-75.7088, 44.1545, (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
 })
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGVsdnYiLCJhIjoiY2szejRmdHFjMWUyajNlcGRwb2dncml0aSJ9.BjzlJbeyH4qtVzr8ns9gDA&limit=1'
-
-request({ url: geocodeURL, json: true }, (error, response) => {
-  if (error){
-    console.log('Unable to connect to location services!')
-  } else if (!response.body.features.length){
-    console.log('Unable to find location')
-  } else {
-    const [ longitude, latitude ] = response.body.features[0].center
-    console.log(`Longitude: ${longitude}, Latitude: ${latitude}`)
-  }
+// callback with two parameters error and response is good practice and strongly recommended
+geocode('Toronto', (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
 })
 
 
